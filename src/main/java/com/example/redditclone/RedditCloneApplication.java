44a3e117f1d007edec4084162b6e2533fa3973be
Nick Hsiao <hsiao.nicholas@gmail.com>
2022-04-03 21:29:04 -0700

    .

--- src/main/java/com/example/redditclone/RedditCloneApplication.java
+++ src/main/java/com/example/redditclone/RedditCloneApplication.java
@@ -2,7 +2,8 @@ package com.example.redditclone;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
+
+
 
 //(exclude = { SecurityAutoConfiguration.class })
 @SpringBootApplication
